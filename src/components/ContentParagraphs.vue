ae7f761cb9a5d1550e0035ddcff4bd3466a61ccd
LeDude <kdelude16@gmail.com>
2019-05-06 00:35:40 -0700

    Created ContentBlock Component
    Created ContentBlock Component inspired by firebase.google.com/use-cases. Added to Services and About pages.
    
    props:
      --Data
        --title - Main title of content block
        --contents
          --contentTitle - Sub-title of a content section
          --contentText - Text of a content section
        --image - an image to display alongside entire content block. If none, and contents.length is less than 4, content sections will be displayed side by side
      --Iterator - Optional parameter to flip image to left or right side of text.
    
    There is only test data in the components.
    
    Data object is in the following format:
    export default {
      data () {
        return {
          cases: [
            {
              title: 'Case Title v2',
              contents: [
                { contentTitle: 'First Point Title', contentText: 'Here is my content' },
                { contentTitle: 'Second Point Title', contentText: 'Here is my second content' }
              ],
              image: 'https://www.w3schools.com/images/colorpicker.gif'
            }
          ]
        }
      },
      components: {
        ContentBlock
      }
    }

---
+++ src/components/ContentParagraphs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="content-block">
+    <p class="content-paragraph pt-4">
+      <strong class="content-title">{{contentTitle}}</strong>
+      <br />
+      <span class="content-text">{{contentText}}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    contentTitle: { type: String },
+    contentText: { type: String }
+  }
+}
+</script>
+
+<style scoped>
+.content-paragraph {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/ServiceTypes.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container id="service-types">
+    <v-layout
+        text-xs-center
+        wrap
+    >
+      <v-flex xs12>
+          <p class="title-main">
+              Services
+          </p>
+      </v-flex>
+    </v-layout>
+    <v-card>
+      <div
+      v-for="(service, index) in services"
+      :key="index"
+      class="service-listing px-4 pt-4">
+        <ContentBlock :data="service"/>
+        <br />
+        <hr v-if='index !== services.length - 1'/>
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import ContentBlock from './ContentBlock'
+
+export default {
+  data () {
+    return {
+      services: [
+        {
+          title: 'Service Title',
+          contents: [
+            { contentTitle: 'First Point Title', contentText: 'Here is my content' },
+            { contentTitle: 'Second Point Title', contentText: 'Here is my second content' }
+          ],
+          image: 'https://www.w3schools.com/images/colorpicker.gif'
+        },
+        {
+          title: 'Second Service Title',
+          contents: [
+            { contentTitle: 'First Point Title', contentText: 'Here is my content' },
+            { contentTitle: 'Second Point Title', contentText: 'Here is my second content' }
+          ],
+          image: 'https://www.w3schools.com/images/colorpicker.gif'
+        }
+      ]
+    }
+  },
+  components: {
+    ContentBlock
+  }
+}
+</script>
+
+<style scoped>
+.service-listing {
+  margin: 10;
+  padding: 10;
+}
+</style>
